d1fb5bcf2038d315acf0fb72b374be1dd5ca9afd
JianRuiqian <jianb1995@hotmail.com>
2017-07-14 21:50:48 +0800

    first commit

---
+++ software/libdmp/dmp.h
@@ -0,0 +1,33 @@
+#ifndef _DMP_H_
+#define _DMP_H_
+
+#define MPU6050
+#define DEFAULT_MPU_HZ  			100
+
+#define DMP_DEBUG
+#ifdef DMP_DEBUG
+#define dmp_log_i(fmt, arg...)      rt_kprintf(fmt, ##arg)
+#define dmp_log_e(fmt, arg...)      rt_kprintf(fmt, ##arg)
+#else
+#define dmp_log_i(fmt, arg...)      do {} while (0)
+#define dmp_log_e(fmt, arg...)      do {} while (0)
+#endif
+
+struct euler_angle
+{
+    float pitch;
+    float roll;
+    float yaw;
+};
+
+void dmp_get_ms(unsigned long *cnt);
+void dmp_delay_ms(unsigned long ms);
+int  dmp_i2c_write(unsigned char slave_addr, unsigned char reg_addr,
+     unsigned char length, unsigned char const *data);
+int  dmp_i2c_read(unsigned char slave_addr, unsigned char reg_addr,
+     unsigned char length, unsigned char *data);
+
+void dmp_get_eulerangle(struct euler_angle *euler);
+void dmp_sys_init(void);
+
+#endif  /* _DMP_H_ */
